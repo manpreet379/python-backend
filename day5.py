bb1ad54991c1c660a25bce8f32596c82abb4fafb
Manpreet Singh <manpreet.singh@quokkalabs.com>
2025-02-17 17:32:17 +0530

    updated task as per tapp

--- day5.py
+++ day5.py
@@ -50,3 +50,92 @@ acc2.withdraw(5000)
 print(f"Total Bank Accounts: {BankAccount.total_accounts}")
 
 
+# class BankAccount:
+#     # Class variable to track the total number of accounts
+#     total_accounts = 0
+
+#     def __init__(self, holder_name, balance, account_type):
+#         self.holder_name = holder_name  # Public instance variable
+#         self.__balance = balance  # Private instance variable
+#         self.account_type = account_type  # Public instance variable
+#         BankAccount.total_accounts += 1  # Increment total accounts
+
+#     # Getter for balance
+#     @property
+#     def balance(self):
+#         return self.__balance
+
+#     # Setter for balance (validates deposit)
+#     @balance.setter
+#     def balance(self, amount):
+#         if amount < 0:
+#             raise ValueError("Balance cannot be negative!")
+#         self.__balance = amount
+
+#     # Method to deposit money (uses setter)
+#     def deposit(self, amount):
+#         if amount > 0:
+#             self.balance += amount  # Using setter
+#             print(f"Deposited ${amount}. New Balance: ${self.balance}")
+#         else:
+#             print("Deposit amount must be positive!")
+
+#     # Method to withdraw money (validates withdrawal)
+#     def withdraw(self, amount):
+#         if 0 < amount <= self.balance:
+#             self.balance -= amount  # Using setter
+#             print(f"Withdrew ${amount}. Remaining Balance: ${self.balance}")
+#         else:
+#             print("Invalid withdrawal amount!")
+
+#     # Getter for account details
+#     @property
+#     def account_details(self):
+#         return f"Account Holder: {self.holder_name}, Type: {self.account_type}, Balance: ${self.balance}"
+
+# # Creating account objects
+# acc1 = BankAccount("Alice", 5000, "Savings")
+# acc2 = BankAccount("Bob", 3000, "Checking")
+
+# # Accessing account details using @property
+# print(acc1.account_details)
+# print(acc2.account_details)
+
+# # Depositing money
+# acc1.deposit(1000)
+
+# # Withdrawing money
+# acc2.withdraw(500)
+
+# # Trying to withdraw more than balance
+# acc2.withdraw(5000)
+
+# # Checking total accounts created
+# print(f"Total Bank Accounts: {BankAccount.total_accounts}")
+
+# # Setting a new balance (using setter)
+# acc1.balance = 7000
+# print(f"Updated Balance for Alice: ${acc1.balance}")
+
+# # Trying to set a negative balance (raises error)
+# # acc1.balance = -500  # Uncomment to see error
+def hollow_hourglass(n):
+    for i in range(n):
+        for j in range((2 * n) - 1):
+            if i == 0 or j == i or j == (2 * n) - 2 - i:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+    for i in range(n-2, -1, -1):
+        for j in range(2 * n - 1):
+            if i == 0 or j == i or j == 2 * n - 2 - i:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+# Set the size of the hourglass
+n = 5
+hollow_hourglass(n)
